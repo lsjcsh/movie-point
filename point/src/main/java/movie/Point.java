d825e3d531ab308a0adcb62ab335d49b0c98126f
이승재 <lsjcsh@naver.com>
2021-02-24 18:08:53 +0900

    point add

---
+++ point/src/main/java/movie/Point.java
@@ -0,0 +1,77 @@
+package movie;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Point_table")
+public class Point {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long bookingId;
+    private Integer score;
+    private String contents;
+    private String status;
+
+    @PostPersist
+    public void onPostPersist(){
+        WaitedPoint waitedPoint = new WaitedPoint();
+        BeanUtils.copyProperties(this, waitedPoint);
+        waitedPoint.setScore(0);
+        waitedPoint.setStatus("Waited Point");
+        waitedPoint.publishAfterCommit();
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        WrittenPoint writtenPoint = new WrittenPoint();
+        BeanUtils.copyProperties(this, writtenPoint);
+        writtenPoint.setScore(100);  //포인트지급
+        writtenPoint.setStatus("Written Point");
+        writtenPoint.publishAfterCommit();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
+}
